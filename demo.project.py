2200dc0450bb216d0637f5155bfd69f25aa28400
preethi <preethi@gmail.com>
2024-09-22 17:35:29 +0530

    v2

--- demo.project.py
+++ demo.project.py
@@ -1,3 +1,22 @@
 # Today we will learn python
 def fun(x,y):
     return x+y
+num = fun(5,2)
+print(num)
+
+def prime(num):
+    if num <= 0:
+        return '0 or -ve numbers are not prime'
+    elif num <= 3:
+        return 'Prime'
+    else:
+        for i in range(2,num):
+            if num % i == 0:
+                return 'Not Prime'
+            return 'Prime'
+            
+   
+print(prime(num))
+
+
+
